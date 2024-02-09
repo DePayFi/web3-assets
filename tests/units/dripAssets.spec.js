5e9bb721e1eb0000d555fa48b6dced6cec1ee3ae
Sebastian Pape <0xneo11@gmail.com>
2024-02-09 07:08:18 +0100

    upgrade to web3-blockchains v9.3.1

--- tests/units/dripAssets.spec.js
+++ tests/units/dripAssets.spec.js
@@ -279,7 +279,7 @@ describe('dripAssets', ()=>{
           balance: '123456789'
         },
         {
-          address: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174',
+          address: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
           symbol: 'USDC',
           name: 'USD Coin',
           decimals: 6,
